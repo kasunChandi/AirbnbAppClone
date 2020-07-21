e8f8dc56686abb66f28d8855783651309f11ece9
kasun Chandimal <39762853+kasunChandi@users.noreply.github.com>
2020-07-21 09:43:27 -0700

    initial commit

---
+++ AirbnbAppClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirbnbAppClone
+//
+//  Created by kc on 7/21/20.
+//  Copyright © 2020 kc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
